5e9797939f86cfca2331922f4cf7a83fbedfd37f
Vldr22 <vldr2212@gmail.com>
2024-12-20 01:01:34 +0300

    add readme

--- src/main/java/com/education/mypaymentservice/MyPaymentServiceApplication.java
+++ src/main/java/com/education/mypaymentservice/MyPaymentServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyPaymentServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MyPaymentServiceApplication.class, args);
     }
