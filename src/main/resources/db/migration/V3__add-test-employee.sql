155df9650e080fea9da18dd423a3f39d037c4002
Vldr22 <vldr2212l@gmail.com>
2025-03-29 19:52:44 +0300

    Миграции БД

---
+++ src/main/resources/db/migration/V3__add-test-employee.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.employees (id, name, surname, midname, email, password, role, created_at, updated_at)
+VALUES
+    (1, 'Владимир', 'Владимирович', 'Путин', 'test@example.com', '$2a$10$CwTycUXWue0Thq9StjUM0uOJ1b9Xq1b1o4yxFMoD2ZcM9TwIrw2Ry
+', 'ROLE_EMPLOYEE', NOW(), NOW());
